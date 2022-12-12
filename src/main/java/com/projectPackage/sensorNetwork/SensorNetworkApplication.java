d27f6ddd22ccec489ec9b739e7cb104d57e515ef
cviverosf <cristofervive@gmail.com>
2022-12-11 21:39:22 -0600

    -COMMIT WORK FILES

---
+++ src/main/java/com/projectPackage/sensorNetwork/SensorNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.projectPackage.sensorNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectPackage/sensorNetwork/SensorNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectPackage.sensorNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
